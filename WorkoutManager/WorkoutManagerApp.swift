b669ea5f471e5d9eebaed0019ff3df282607074e
matsuei <matuse.kenta@gmail.com>
2021-04-04 11:03:24 +0900

    Initial Commit

---
+++ WorkoutManager/WorkoutManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutManagerApp.swift
+//  WorkoutManager
+//
+//  Created by 松栄健太 on 2021/04/04.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
